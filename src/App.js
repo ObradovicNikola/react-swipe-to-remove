11d89a2a4813614d58dcf169557d65f366ace955
ObradovicNikola <obradovicnikola009@gmail.com>
2019-09-12 10:40:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import SwipeableListItem from "./SwipeableList/SwipeableListItem";
+import SwipeableList from "./SwipeableList/SwipeableList";
+import "./App.css";
+
+const background = <span>Archive</span>;
+const fakeContent = (
+  <div className="FakeContent">
+    <span>Swipe to delete</span>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SwipeableList background={background}>
+          <SwipeableListItem>{fakeContent}</SwipeableListItem>
+          <SwipeableListItem>{fakeContent}</SwipeableListItem>
+          <SwipeableListItem>{fakeContent}</SwipeableListItem>
+          <SwipeableListItem>{fakeContent}</SwipeableListItem>
+          <SwipeableListItem>{fakeContent}</SwipeableListItem>
+        </SwipeableList>
+      </div>
+    );
+  }
+}
+
+export default App;
